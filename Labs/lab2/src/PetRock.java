a2ce18af88ea20737603e9289cfc63f78c8366cc
jingjinglin <lin.jingj@northeastern.edu>
2021-05-27 14:06:24 -0700

    PetRock files with finished 4 parts

--- Labs/lab2/src/PetRock.java
+++ Labs/lab2/src/PetRock.java
@@ -3,13 +3,22 @@ import java.lang.reflect.Constructor;
 public class PetRock {
     private String name;
     private boolean happy = false;
+    private int mass = 0;
 
-    public PetRock(String name) {
+    public PetRock(String name, int mass) {
+        if (name.isEmpty()){
+            throw new IllegalArgumentException();
+        }
         this.name = name;
+        this.mass = mass;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getMass() {
+        return mass;
     }
 
     public boolean isHappy() {
@@ -18,5 +27,26 @@ public class PetRock {
 
     public void playWithRock() {
         happy = true;
+    }
+
+    public String getHappyMessage(){
+        if (!happy) {
+            throw new IllegalStateException();
+        }
+        return "I'm happy!";
+    }
+
+    public String toString(){
+        return name+" "+mass;
+    }
+
+    public int getFavNumber(){
+        return 42;
+    }
+
+    public void waitTillHappy(){
+        while (!happy) {
+            // do nothing!
+        }
     }
 }
--- Labs/lab2/test/PetRockTest.java
+++ Labs/lab2/test/PetRockTest.java
@@ -1,9 +1,22 @@
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 import static org.junit.Assert.*;
 
 public class PetRockTest {
-    private PetRock rocky = new PetRock("Rocky");
+    private PetRock rocky = new PetRock("Rocky", 1);
+
+    // Timeout rule:
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(10);
+
+    @Before
+    public void myTestSetUp() throws Exception {
+        rocky = new PetRock("Rocky", 1);
+    }
 
     @Test
     public void getName() throws Exception {
@@ -19,5 +32,45 @@ public class PetRockTest {
     public void testHappyAfterPlay() throws Exception{
         rocky.playWithRock();
         assertTrue(rocky.isHappy());
+    }
+
+    @Test
+    public void name() throws Exception {
+        rocky.playWithRock();
+        String msg = rocky.getHappyMessage();
+        assertEquals("I'm happy!", msg);
+
+    }
+
+    @Test
+    public void testFavNum() throws Exception {
+        assertEquals(42, rocky.getFavNumber());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void emptyNameFail() throws Exception {
+        new PetRock("", 1);
+    }
+
+    @Test(timeout = 100) // in ms
+    public void waitForHappyTimeout() throws Exception {
+        rocky.waitTillHappy();
+    }
+
+    @Test
+    public void testMass() throws Exception {
+        assertEquals(1, rocky.getMass());
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        assertEquals("Rocky 1", rocky.toString());
+    }
+
+
+
+    @Test (expected = IllegalStateException.class)
+    public void testUnHappy() throws Exception {
+        rocky.getHappyMessage();
     }
 }
