7fa80c3e50ef8559876fc22bc25a1ff478f905fe
jingjinglin <lin.jingj@northeastern.edu>
2021-06-11 00:18:38 -0700

    lab 4 finished

---
+++ Labs/lab4/src/SListImpl.java
@@ -0,0 +1,143 @@
+public class SListImpl implements SList {
+    // Only need the head as the member varialbe.
+    // As long as I have the head, I'll be able find
+    // all the nodes, following the next pointers.
+    private Node head;
+
+    // creates an empty linked list
+    public SListImpl() {
+    }
+
+    // creates a linked list with head n
+    public SListImpl(Node n) {
+        head = n;
+    }
+
+    @Override
+    public boolean add(String val) {
+        Node n=new Node(val, null);
+        Node traversePtr = head;
+        if(isEmpty()) {
+            head = n;
+        } else {
+            while (traversePtr != null) { // we jump out of the loop when we're at the null ptr
+                if (traversePtr.next == null) {
+                    traversePtr.next = n;
+                    break;
+                }
+                // jump to the next node
+                traversePtr = traversePtr.next;
+            }
+        }
+        return true;
+
+    }
+
+    @Override
+    public String get(int index) {
+        int counter = 0;
+        Node traversePtr = head;
+
+        String val= null;
+        while(traversePtr != null) { // we jump out of the loop when we're at the null ptr
+            if(counter == index){
+                val = traversePtr.val;
+            }
+            // increment the counter
+            counter++;
+            // jump to the next node
+            traversePtr = traversePtr.next;
+        }
+        return val;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public String remove(int index) {
+        int counter = 0;
+        Node traversePtr = head;
+        Node PrePtr = head;
+        String val= null;
+
+        while(traversePtr != null) { // we jump out of the loop when we're at the null ptr
+            if(counter == index){
+                 val = traversePtr.val;
+                 if (traversePtr == head) {
+                     head = traversePtr.next;
+                 } else {
+                     PrePtr.next = traversePtr.next;
+                 }
+            }
+            // increment the counter
+            counter++;
+            // jump to the next node
+            PrePtr = traversePtr;
+            traversePtr = traversePtr.next;
+        }
+        return val;
+    }
+
+    @Override
+    public int size() {
+        int counter = 0;
+        Node traversePtr = head;
+
+        while(traversePtr != null) { // we jump out of the loop when we're at the null ptr
+            // increment the counter
+            counter++;
+            // jump to the next node
+            traversePtr = traversePtr.next;
+        }
+
+        return counter;
+    }
+
+    @Override
+    public SList oddWords(){
+        SList list = new SListImpl();
+        Node traversePtr = head;
+
+        while(traversePtr != null) { // we jump out of the loop when we're at the null ptr
+            if(traversePtr.val.length() % 2 == 1){
+                list.add(traversePtr.val);
+            }
+            // jump to the next node
+            traversePtr = traversePtr.next;
+        }
+        return list;
+    }
+
+    @Override
+    public SList evenWords(){
+        SList list = new SListImpl();
+        Node traversePtr = head;
+
+        while(traversePtr != null) { // we jump out of the loop when we're at the null ptr
+            if(traversePtr.val.length() % 2 == 0){
+                list.add(traversePtr.val);
+            }
+            // jump to the next node
+            traversePtr = traversePtr.next;
+        }
+        return list;
+    }
+
+
+    @Override
+    public String toString() {
+        String res = "";
+        Node traversePtr = head;
+
+        while(traversePtr != null) { // we jump out of the loop when we're at the null ptr
+            res = res + traversePtr.toString();
+            // jump to the next node
+            traversePtr = traversePtr.next;
+        }
+
+        return res;
+    }
+}
---
+++ Labs/lab4/test/SListTest.java
@@ -0,0 +1,23 @@
+import java.net.PortUnreachableException;
+
+public class SListTest {
+    public static void main(String[] args) {
+        SList list = new SListImpl();
+        list.add("java");
+        list.add("is");
+        list.add("really");
+        list.add("hard!");
+
+        for (int i = 0; i < list.size(); i++) {
+          System.out.println(list.get(i));
+        }
+
+        list.remove(2);
+
+        System.out.println(list);
+        System.out.println(list.oddWords());
+        System.out.println(list.evenWords());
+
+
+    }
+}
