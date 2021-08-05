006eba8aaa82e006389c92e15b684a35fa008fcf
jingjinglin <lin.jingj@northeastern.edu>
2021-08-04 23:35:05 -0700

    update lab11

---
+++ Labs/lab11/src/DonateJustGiving.java
@@ -0,0 +1,7 @@
+public class DonateJustGiving implements DonationStrategy {
+
+    @Override
+    public void donate(int amount) {
+        System.out.println("Processing $" + amount + " donation through JustGiving.");
+    }
+}
---
+++ Labs/lab11/src/DonatePayPal.java
@@ -0,0 +1,7 @@
+public class DonatePayPal implements DonationStrategy {
+
+    @Override
+    public void donate(int amount) {
+        System.out.println("Processing $" + amount + " donation through PayPal.");
+    }
+}
